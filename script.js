fb87bb765ec4ff816a187b7930ca7e2b4b41508d
xiadou1043 <55755980+xiadou1043@users.noreply.github.com>
2020-12-18 00:59:01 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul=document.querySelector("ul");
+
+function inputLength(){
+    return input.value.length;
+}
+function createListElement(){
+     var li = document.createElement("li");
+     li.appendChild(document.createTextNode(input.value))
+     ul.append(li);
+     input.value="";
+}
+function addListAfterClick(){
+    if(inputLength() > 0){
+        createListElement();
+    }
+}
+function addListAfterKeypress(event){
+    if(inputLength() > 0 && event.keycode === 13){
+        createListElement();
+    }
+}
+button.addEventListener("click",addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
